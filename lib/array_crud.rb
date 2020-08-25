fcae6d306049c032fadf364065f5d106367f33ba
Taylor Sieling <taylorasieling@gmail.com>
2020-08-25 02:13:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = []
-  array.unshift ("element")
+  array.unshift ("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
