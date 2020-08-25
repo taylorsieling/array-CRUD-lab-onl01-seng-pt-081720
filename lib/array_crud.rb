4499c0d8c6bd8ddb03b650dfc9c5e86f5bd52ce2
Taylor Sieling <taylorasieling@gmail.com>
2020-08-25 02:20:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,8 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = []
-  array.pop ("")
+  array = ["Pike", "Percy", "Luna"]
+  array.pop ("Luna")
 end
 
 def remove_element_from_start_of_array(array)
