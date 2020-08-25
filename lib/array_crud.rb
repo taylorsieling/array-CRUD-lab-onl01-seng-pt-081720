759e90cdbe539c0a8a882b28c45e1fadfab2bc62
Taylor Sieling <taylorasieling@gmail.com>
2020-08-25 02:11:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["Pike", "Percy", "Luna", "Felix"]
+  array.unshift ("element")
 end
 
 def add_element_to_start_of_array(array, element)
